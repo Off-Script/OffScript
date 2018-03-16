978053548acf679f62cd464b284e8533d0f03560
Annah Patterson Pelin <annah.pelin@gmail.com>
2018-03-16 14:04:56 -0400

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015', 'react']
+      }
+    },
+    {
+      test: /\.css$/,
+      loader: 'css-loader'
+    }]
+  }
+};
