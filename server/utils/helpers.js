49d78c3e23a8295f53508d1196f30f5c9e604130
rhmorrow <ryanhmorrow@gmail.com>
2018-03-16 15:16:49 -0700

    Added natural language helper

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -1,5 +1,7 @@
 let ToneAnalyzer = require("watson-developer-cloud/tone-analyzer/v3")
 let toneAPI = require ("../../config/toneAPI.js")
+let NatLang = require('watson-developer-cloud/natural-language-understanding/v1.js');
+let natlangAPI = require('../../config/natlangAPI.js')
 
  let toneAnalyzer = new ToneAnalyzer({
   username: toneAPI.credentials.username,
@@ -8,4 +10,12 @@ let toneAPI = require ("../../config/toneAPI.js")
   url: 'https://gateway.watsonplatform.net/tone-analyzer/api/'
 });
 
-module.exports = toneAnalyzer;
+ let natLang = new NatLang({
+  username: natlangAPI.credentials.username,
+  password: natlangAPI.credentials.password,
+  version: '2016-05-19',
+  url:  "https://gateway.watsonplatform.net/natural-language-understanding/api"
+});
+
+module.exports.toneAnalyzer = toneAnalyzer;
+module.exports.natLang = natLang;
