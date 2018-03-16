1f5d7b170ee2cc6bc8b25d48469fa81d5c334ec7
Yufan Wang <yufansmail@gmail.com>
2018-03-16 16:57:58 -0700

    webpack 4 fix

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path')
 const bodyParser = require('body-parser'); 
-const morgan = require('morgan'); 
 let pg = require('pg'); 
 // let db = require('../database/index.js');
 
@@ -9,15 +8,14 @@ const app = express();
 
 let PORT = process.env.PORT || 3000;
 
-app.use(morgan);
 app.use(bodyParser.json());
 
 // Serves static files to client
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // wild card routing all pages to the React Router
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(PORT, function() {
