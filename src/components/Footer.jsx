1938d2214879190cfaefc9022fb5f545608d92b0
rhmorrow <ryanhmorrow@gmail.com>
2018-03-19 19:03:31 -0700

    Made footer boilerplate not orange

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const Footer = (props) => (
-  <footer className='page-footer'>
-    <div className='container'>
-      <div className='row'>
-        <div className='col s9'><h4>Offscript Team</h4></div>
-        <div className='col s3'>Follow Us</div>
-      </div>
+  <footer>
+    <div className='row'>
+      <div className='col s9'><h4>Offscript Team</h4></div>
+      <div className='col s3'>Follow Us</div>
     </div>
   </footer>
 )
