da11722ee856f815c16bd7441d977ba800daacc1
rhmorrow <ryanhmorrow@gmail.com>
2018-03-20 14:39:53 -0700

    Script comparison function renders errors in transcript

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Interweave from 'interweave';
+import scriptComparison from '../lib/ScriptComparison.js'
 
 const Results  = (props) => (
   <div>
@@ -11,7 +13,9 @@ const Results  = (props) => (
       </div>
       <div className="col s6">
         <h3>Your Transcript</h3>
-        <p>{props.transcript}</p>
+        <Interweave
+          tagName="p"
+          content={scriptComparison(props.script, props.transcript)} />
       </div>
       <h3>Speech Analysis</h3>
         <p>insert graph here</p>
