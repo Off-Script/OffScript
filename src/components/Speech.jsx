47dff30d3ad5cfff6f1a7437df0ba6ddb11f49b4
Yufan Wang <yufansmail@gmail.com>
2018-03-19 17:55:16 -0700

    voice recognition

--- src/components/Speech.jsx
+++ src/components/Speech.jsx
@@ -1,19 +1,57 @@
 import React from 'react';
+import VoiceRecognition from '../lib/VoiceRecognition.js'
 
 class Speech extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      start: false,
+      stop: false,
+      transcript: ''
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    this.onResult = this.onResult.bind(this);
   }
 
   handleSubmit(e) {
-    this.props.history.push('/results');
+    this.onEnd();
+    this.props.history.push('/');
+  }
+
+  onEnd() {
+    this.setState({ start: false, stop: false });
+  }
+
+  onError() {
+    console.log('error');
+  }
+
+  onResult(transcript) {
+    var result = transcript.finalTranscript;
+    var current = this.state.transcript;
+    this.setState({
+      transcript: current + result + '.'
+    });
+    console.log(result);
   }
 
   render() {
     return (
       <div>
+        <button onClick={() => this.setState({ start: true })}>start</button>
+        <button onClick={() => this.setState({ stop: true })}>stop</button>
+        {this.state.start && (
+          <VoiceRecognition
+            continuous={true}
+            onResult={this.onResult}
+            onEnd={this.onEnd}
+            lang="en-us"
+            stop={this.state.stop}
+          />
+        )}
         <button onClick={this.handleSubmit}>Results</button>
+        <p className="flow-text">{this.state.transcript}</p>
       </div>
     )
 
