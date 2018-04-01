b7f88384af0c1d4c02bc1114dd3d7d0b2207b12c
Yufan Wang <yufansmail@gmail.com>
2018-04-01 00:11:19 -0700

    added text shadow

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -30,7 +30,7 @@ class Landing extends React.Component {
           <div className="parallax-container highlight valign-wrapper">
             <div className="container">
               <h3 className="valign cyan-text">Get OffScript</h3>
-              <div className="cyan-text flow-text"><b>Write a speech, upload a document from your computer, or practice from our free and open Library of famous works. We'll listen to you, track your accuracy, and show you what sections to work on.</b></div>
+              <div className="landing-text cyan-text flow-text"><b>Write a speech, upload a document from your computer, or practice from our free and open Library of famous works. We'll listen to you, track your accuracy, and show you what sections to work on.</b></div>
             </div>
             <div className="parallax"><img src="media/work.jpg" /></div>
           </div>
@@ -40,7 +40,7 @@ class Landing extends React.Component {
           <div className="parallax-container highlight valign-wrapper">
             <div className="container">
               <h3 className="valign white-text">Go OffScript</h3>
-                <div className="white-text flow-text"><b>Sometimes an unscripted moment can make your speech shine. With tone analytics from Watson, you can compare your performance to your original script and make changes as you improvise.</b></div>
+              <div className="landing-text white-text flow-text"><b>Sometimes an unscripted moment can make your speech shine. With tone analytics from Watson, you can compare your performance to your original script and make changes as you improvise.</b></div>
             </div>
             <div className="parallax"><img src="media/stage.jpg" /></div>
           </div>
@@ -50,7 +50,7 @@ class Landing extends React.Component {
           <div className="parallax-container highlight valign-wrapper">
             <div className="container">
               <h3 className="valign black-text">Stay OffScript</h3>
-              <div className="black-text flow-text"><b>Practice makes perfect. With a free OffScript account, you'll be ready to edit and save your scripts so you can track your improvement over time.</b></div>
+              <div className="landing-text black-text flow-text"><b>Practice makes perfect. With a free OffScript account, you'll be ready to edit and save your scripts so you can track your improvement over time.</b></div>
               <a className="btn waves-effect cyan hoverable modal-trigger" href="#modal-login">Login / Signup</a>
             </div>
             <div className="parallax"><img src="media/leaves.jpg" /></div>
