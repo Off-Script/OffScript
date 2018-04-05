e00e1680f846bb14fe451a946d9746bbf4ba6c18
rhmorrow <ryanhmorrow@gmail.com>
2018-04-05 16:43:00 -0700

    Added consistent headers to views. Line Reader now only accepts valid scripts. User explanations added.

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -48,13 +48,15 @@ class Upload extends React.Component {
       this.props.setlines(this.state.script);
     } else {
       let editor = this.reactQuillRef.getEditor();
-      if(editor.getText().length > 50) {
-        let script = editor.getText();
-        this.setState({script});
-        this.props.setlines(script);
-        this.clear();
-      } else {
+      let text = editor.getText();
+      if(text.length < 50) {
         Materialize.toast("For best results, script must be 50 characters or more", 2000);
+      } else if( text.split(/.[A-Z]{2,100}[^a-z]/).length <= 1 ) {
+        Materialize.toast("For line reader feature, capitalize and separate character names", 4000);
+      } else {
+        this.setState({script: text});
+        this.props.setlines(text);
+        this.clear();
       }
     }
   }
@@ -79,42 +81,44 @@ class Upload extends React.Component {
   }
 
   render() {
-    return (
-      <div className="container">
-        <div className="card upload-card">
-          <div className="card-tabs">
-            <ul className="tabs tabs-fixed-width grey lighten-3">
-              <li className="tab">
-                <a className="cyan-text" href="#type">
-                  Type a script
-                </a>
-              </li>
-              <li className="tab">
-                <a className="cyan-text" href="#upload">
-                  Upload a script
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div id="type" className="input-field upload">
-            <ReactQuill 
-              ref={(el) => { this.reactQuillRef = el; }}
-              theme="snow"
-              value={this.state.script} 
-            />
-          </div>
-          <div id="upload" className="upload">
-            <FileUpload onChange={this.readFile.bind(this)}/>
-          </div>
-        </div>
-        <button className="waves-effect btn cyan accent-4 hoverable" onClick={this.handleSubmit}>
-          <i className="material-icons left">file_upload</i>Memorize Speech
-        </button>
-        <button className="waves-effect btn cyan accent-4 hoverable" onClick={this.lineReader}>
-          <i className="material-icons left">tag_faces</i>Run Lines
-        </button>
+    return <div className="container">
+      <div className="row">
+        <h1 className="cyan-text">Create Script</h1>
       </div>
-    );
+      <div className="card upload-card">
+        <div className="card-tabs">
+          <ul className="tabs tabs-fixed-width grey lighten-3">
+            <li className="tab">
+              <a className="cyan-text" href="#type">
+                  Type a script
+              </a>
+            </li>
+            <li className="tab">
+              <a className="cyan-text" href="#upload">
+                  Upload a script
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="type" className="input-field upload">
+          <ReactQuill ref={el => {
+            this.reactQuillRef = el;
+          }} theme="snow" value={this.state.script} />
+        </div>
+        <div id="upload" className="upload">
+          <FileUpload onChange={this.readFile.bind(this)} />
+        </div>
+      </div>
+      <button className="waves-effect btn cyan accent-4 hoverable" onClick={this.handleSubmit}>
+        <i className="material-icons left">record_voice_over</i>Speech Analysis
+      </button>
+      <button className="waves-effect btn cyan accent-4 hoverable" onClick={this.lineReader}>
+        <i className="material-icons left">tag_faces</i>Line Reader
+      </button>
+      <i className="material-icons center cyan-text tooltipped" data-position="top" data-delay="50" data-tooltip="To upload dialogue for the Line Reader, capitalize your character names and separate them with line breaks.">
+          help
+      </i>
+    </div>;
   }
 }
 
